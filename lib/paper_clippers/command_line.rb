573c205aa234b08c25aff64124d01f14ebfe53f9
kojix2 <2xijok@gmail.com>
2024-02-04 03:29:55 +0900

    Improved error message

--- lib/paper_clippers/command_line.rb
+++ lib/paper_clippers/command_line.rb
@@ -6,7 +6,7 @@ class PaperClipper
     def initialize
       @options = {}
 
-      OptionParser.new do |opts|
+      @parser = OptionParser.new do |opts|
         opts.banner = "kirinuki [options]"
         opts.separator ""
         opts.separator "Example: ruby kirinuki.rb -f 'path/to/your.html' -p '//*[@id=\"sec数\"]' -r '1..12'"
@@ -17,14 +17,17 @@ class PaperClipper
         opts.on("-o", "--output OUTPUT_DIR", "Output directory") do |v|
           @options[:output_dir] = v
         end
-      end.parse!
+      end
+      
+      @parser.parse!
     end
 
     def run
       clipper = PaperClipper.new(@options[:html_path], @options[:pattern], @options[:range_str], @options[:output_dir])
       clipper.clip # renaming execute to clip is more appropriate in this case
     rescue StandardError => e
-      warn(e.message)
+      warn(@parser)
+      warn("\n[kirinuki] Error: #{e.message} (#{e.class}) (#{e.backtrace.first})")
       exit 1
     end
   end
