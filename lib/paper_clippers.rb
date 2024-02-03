aac6f3a494df6e407cae1a8e45a2de7046a078d2
kojix2 <2xijok@gmail.com>
2024-02-04 00:31:05 +0900

    Add code written by ChatGPT

--- lib/paper_clippers.rb
+++ lib/paper_clippers.rb
@@ -1,7 +1,44 @@
-# frozen_string_literal: true
+require "nokogiri"
+require "fileutils"
 
-require_relative "paper_clippers/version"
+class PaperClipper
+  def initialize(html_path, pattern, range_str, output_dir)
+    @html_path = html_path
+    @pattern = pattern
+    @range_str = range_str
+    @output_dir = output_dir || File.basename(html_path, ".*")
 
-module PaperClippers
-  class Error < StandardError; end
+    raise "No 数 found. Please check the pattern." unless pattern.include? "数"
+  end
+
+  def execute
+    doc = Nokogiri::HTML(open(@html_path))
+    range = eval(@range_str)
+
+    range.each do |i|
+      xpath = @pattern.gsub("数", i.to_s)
+      nodes = doc.xpath(xpath)
+
+      # process each node
+      nodes.each do |node|
+        html = process_html(node.inner_html, xpath)
+        file_name = xpath.gsub(/[^0-9A-Za-z_]/, "")
+
+        FileUtils.mkdir_p(@output_dir)
+        File.open("#{@output_dir}/#{file_name}.txt", "a") do |f|
+          f.puts html
+        end
+      end
+    end
+  end
+
+  private
+
+  def process_html(html, _xpath)
+    html.gsub!(/\R|\t/, " ") # remove any existing newlines or tabs
+    html.gsub!(/<(h[1-6]|p|li|dd)/, "\n\\0") # add a newline before each heading and paragraph
+    html.gsub!(%r{</(h[1-6]|p|li|dd)>}, "\\0\n") # add a newline after each ending heading and paragraph tag
+
+    Nokogiri::HTML(html).text
+  end
 end
---
+++ lib/paper_clippers/command_line.rb
@@ -0,0 +1,28 @@
+require "optparse"
+require "paper_clipper"
+
+class PaperClipper
+  class CommandLine
+  def initialize
+    @options = {}
+
+    OptionParser.new do |opts|
+      opts.banner = "kirinuki [options]"
+      opts.separator ""
+      opts.separator "Example: ruby kirinuki.rb -f 'path/to/your.html' -p '//*[@id=\"sec数\"]' -r '1..12'"
+      opts.on("-f", "--file HTML_PATH", "HTML file path") { |v| @options[:file] = v }
+      opts.on("-p", "--pattern PATTERN", "Pattern") { |v| @options[:pattern] = v }
+      opts.on("-r", "--range RANGE", "Range") { |v| @options[:range] = v }
+      opts.on("-o", "--output OUTPUT_DIR", "Output directory") { |v| @options[:output] = v }
+    end.parse!
+  end
+
+  def run
+    clipper = PaperClipper.new(@options[:file], @options[:pattern], @options[:range], @options[:output])
+    clipper.execute
+  rescue => e
+    warn(e.message)
+    exit 1
+  end
+end
+
