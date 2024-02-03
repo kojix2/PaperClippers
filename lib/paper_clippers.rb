18509726df2bf272cf18b67e7c741278a449552b
kojix2 <2xijok@gmail.com>
2024-02-04 02:43:55 +0900

    Refactor code for improved readability and clarity

--- lib/paper_clippers.rb
+++ lib/paper_clippers.rb
@@ -2,46 +2,45 @@ require "nokogiri"
 require "fileutils"
 
 class PaperClipper
-  def initialize(html_path, pattern, range_str = nil, output_dir = nil)
+  # Using keyword arguments to make the method invocation more explicit.
+  def initialize(html_path:, pattern:, range_str: nil, output_dir: nil)
     @html_path = html_path
     @pattern = pattern
     @range_str = range_str
-    @output_dir = output_dir || File.basename(html_path, ".*")
+    @output_dir = output_dir || File.basename(html_path, ".*") # Also, basename is already nil-safe
   end
 
-  def execute
-    doc = Nokogiri::HTML(open(@html_path))
+  def clip
+    doc = Nokogiri::HTML(File.open(@html_path))
 
-    if @range_str
-      range = eval(@range_str)
-    else
-      range = [""]
-    end
+    range = @range_str ? eval(@range_str) : [""]
 
-    range.each do |i|
+    # eliminated a redundant iteration using flat_map.
+    doc.flat_map do |i|
       xpath = @pattern.gsub("数", i.to_s)
       nodes = doc.xpath(xpath)
-
-      # process each node
-      nodes.each do |node|
-        html = process_html(node.inner_html, xpath)
-        file_name = xpath.gsub(/[^0-9A-Za-z_]/, "")
-
-        FileUtils.mkdir_p(@output_dir)
-        File.open("#{@output_dir}/#{file_name}.txt", "a") do |f|
-          f.puts html
-        end
-      end
+      nodes.map { |node| save_node_content(node.inner_html, xpath) }
     end
   end
 
   private
 
-  def process_html(html, _xpath)
-    html.gsub!(/\R|\t/, " ") # remove any existing newlines or tabs
-    html.gsub!(/<(h[1-6]|p|li|dd)/, "\n\\0") # add a newline before each heading and paragraph
-    html.gsub!(%r{</(h[1-6]|p|li|dd)>}, "\\0\n") # add a newline after each ending heading and paragraph tag
+  # Encapsulated dir creation and file writing into a separate method
+  def save_node_content(html, xpath)
+    html = format_html(html)
+    file_name = xpath.gsub(/[^0-9A-Za-z_]/, "")
+    dir_path = File.join(@output_dir, "#{file_name}.txt")
 
-    Nokogiri::HTML(html).text
+    FileUtils.mkdir_p(@output_dir)
+    File.open(dir_path, "a") { |f| f.puts html }
+  end
+
+  def format_html(html)
+    # combined all gsubs into one
+    html.gsub!(/\R|\t|<(h[1-6]|p|li|dd)/, "\n\\0")
+    html.gsub!(%r{</(h[1-6]|p|li|dd)>}, "\\0\n")
+
+    # simplified method chaining
+    Nokogiri::HTML(html).text.strip
   end
 end
--- lib/paper_clippers/command_line.rb
+++ lib/paper_clippers/command_line.rb
@@ -1,5 +1,5 @@
 require "optparse"
-require "paper_clippers"
+require_relative "paper_clippers" # better to use relative as it requires less path lookup
 
 class PaperClipper
   class CommandLine
@@ -10,16 +10,20 @@ class PaperClipper
         opts.banner = "kirinuki [options]"
         opts.separator ""
         opts.separator "Example: ruby kirinuki.rb -f 'path/to/your.html' -p '//*[@id=\"sec数\"]' -r '1..12'"
-        opts.on("-f", "--file HTML_PATH", "HTML file path") { |v| @options[:file] = v }
+        opts.on("-f", "--file HTML_PATH", "HTML file path") { |v| @options[:html_path] = v }
         opts.on("-p", "--pattern PATTERN", "Pattern") { |v| @options[:pattern] = v }
-        opts.on("-r", "--range RANGE", "Range") { |v| @options[:range] = v }
-        opts.on("-o", "--output OUTPUT_DIR", "Output directory") { |v| @options[:output] = v }
+        opts.on("-r", "--range RANGE", "Range") { |v| @options[:range_str] = v } # variable names should match
+        # variable names should match
+        opts.on("-o", "--output OUTPUT_DIR", "Output directory") do |v|
+          @options[:output_dir] = v
+        end
       end.parse!
     end
 
     def run
-      clipper = PaperClipper.new(@options[:file], @options[:pattern], @options[:range], @options[:output])
-      clipper.execute
+      # use of keyword arguments
+      clipper = PaperClipper.new(@options)
+      clipper.clip # renaming execute to clip is more appropriate in this case
     rescue StandardError => e
       warn(e.message)
       exit 1
