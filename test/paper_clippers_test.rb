ff4dda9f74888c43d3eb9e8f155036769777b598
kojix2 <2xijok@gmail.com>
2024-02-04 12:15:41 +0900

    Add setup and teardown methods to the PaperClippersTest class

--- test/paper_clippers_test.rb
+++ test/paper_clippers_test.rb
@@ -1,8 +1,16 @@
 require_relative "test_helper"
-
 require "tmpdir"
 
 class PaperClippersTest < Test::Unit::TestCase
+  def setup
+    @html_path = File.expand_path("fixtures/test.html", __dir__)
+    @output_dir = Dir.mktmpdir
+  end
+
+  def teardown
+    FileUtils.rm_r(@output_dir)
+  end
+
   test "VERSION" do
     assert do
       ::PaperClippers.const_defined?(:VERSION)
@@ -10,49 +18,35 @@ class PaperClippersTest < Test::Unit::TestCase
   end
 
   test "clip method should save node content to file without range" do
-    # Prepare test data
-    html_path = File.expand_path("fixtures/test.html", __dir__)
     xpath = "//div[@class='summary']"
-    output_dir = Dir.mktmpdir
-
-    clipper = PaperClipper.new(html_path, xpath, nil, output_dir, nil)
+    clipper = PaperClipper.new(@html_path, xpath, nil, @output_dir, nil)
     clipper.clip
 
     expected_contents = "Summary\n" +
                         "     \n" +
                         "This is a summary of the paper.\n"
-    file_path = File.join(output_dir, "divclasssummary.txt")
+    file_path = File.join(@output_dir, "divclasssummary.txt")
     assert File.exist?(file_path)
     assert_equal expected_contents, File.read(file_path)
   end
 
   test "clip method should save node content to file with range" do
-    # Prepare test data
-    html_path = File.expand_path("fixtures/test.html", __dir__)
     xpath = "//div[@class='sec指']"
     range_str = "1..2"
-    output_dir = Dir.mktmpdir
     replace_str = "指"
 
-    # Create a PaperClipper instance
-    clipper = PaperClipper.new(html_path, xpath, range_str, output_dir, replace_str)
-
-    # Call the clip method
+    clipper = PaperClipper.new(@html_path, xpath, range_str, @output_dir, replace_str)
     clipper.clip
 
-    # Verify the files are created and contain the expected content
     expected_contents = [
       "Lorem ipsum dolor sit amet\n",
       "consectetur adipiscing elit\n"
     ]
 
     expected_contents.each_with_index do |expected_content, index|
-      file_path = File.join(output_dir, "divclasssec#{index + 1}.txt")
+      file_path = File.join(@output_dir, "divclasssec#{index + 1}.txt")
       assert File.exist?(file_path)
       assert_equal expected_content, File.read(file_path)
     end
-
-    # Clean up
-    FileUtils.rm_r(output_dir)
   end
 end
